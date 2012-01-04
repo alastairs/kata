6c8e47d2301aff6751df4039cc1b52fb2f801374
Alastair Smith <github@alastairsmith.me.uk>
2012-01-04 18:30:26 +0000

    Add first test.

---
+++ Anagrams/Anagrams.Tests/AnagramGenerator_Generate_Should.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Anagrams.Tests
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class AnagramGenerator_Generate_Should
+    {
+        [Test]
+        public void ReturnAnEmptyCollection_WhenGivenAnEmptyString()
+        {
+            var anagrams = new Anagrams.AnagramGenerator();
+
+            var generatedAnagrams = anagrams.Generate(string.Empty);
+
+            CollectionAssert.IsEmpty(generatedAnagrams);
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
